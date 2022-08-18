0d9f6a4c5ffe488820e3a497d5a53faa28d3239a
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-08-18 23:14:13 +0300

    Improve algorimt

--- src/sorting/src/lib/counting/index.ts
+++ src/sorting/src/lib/counting/index.ts
@@ -2,19 +2,22 @@ import { ICountingSorting } from './model'
 
 export class CountingSorting implements ICountingSorting {
   simple (arr: number[], max: number): number[] {
+    const n = arr.length
+
+    const sortedArr = Array.from({ length: arr.length }, (_, i) => 0)
     const count = Array.from({ length: max }, (_, i) => 0)
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < n; i++) {
       ++count[arr[i]]
     }
 
-    const sortedArr: number[] = []
+    for (let i = 1; i <= max; ++i) {
+      count[i] += count[i - 1]
+    }
 
-    for (let i = 0; i < max; i++) {
-      while (count[i] > 0) {
-        sortedArr.push(i)
-        count[i]--
-      }
+    for (let i = n - 1; i >= 0; i--) {
+      sortedArr[count[arr[i]] - 1] = arr[i]
+      --count[arr[i]]
     }
 
     return sortedArr
